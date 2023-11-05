c9c9b019063b41c62a05dcb7c3fb8b9a33bd84ef
Gabriel Moscarde <78855746+Moscarde@users.noreply.github.com>
2023-11-05 08:15:46 -0300

    Aula 2

---
+++ Aula1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+    int valor;
+    printf("Digite um numero:");
+    scanf("%d", &valor);
+    printf("\n o numero eh: %d", valor);
+}
---
+++ Aula2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    
+    do{
+       printf("\n Digite um numero do sabor: \n");
+    
+        printf("\t (1) ..flocos\n");
+        printf("\t (2) ..morango\n");
+        printf("\t (3) ..baunilha\n");
+
+        scanf("%d", &i); 
+    }while((i<1) || (i>3));
+    
+    switch(i){
+        case 1:
+            printf("\t\t Voce escolheu flocos");
+            break;
+            
+        case 2:
+            printf("\t\t Voce escolheu morango");
+            break;
+            
+        case 3:
+            printf("\t\t Voce escolheu baunilha");
+            break;
+    }
+    return 0;
+}
